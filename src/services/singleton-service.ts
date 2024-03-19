35b28a6543f8b499284d31361a6beb0a83e9ef3c
BipanKishore <bipankishore@gmail.com>
2024-03-19 18:27:43 +0530

    createService

--- src/services/singleton-service.ts
+++ src/services/singleton-service.ts
@@ -1,20 +1,16 @@
 import {IResizableContext} from '../@types'
-import { noop } from '../utils/util'
-
-type ICreateService = () => IResizableContext
 
 function SingletonService () {
   const serviceMap: {
     [key: string]: IResizableContext
   } = {}
 
-  const getService = (id: string, createService: ICreateService) => 
-     serviceMap[id] = serviceMap[id] ? serviceMap[id] : (createService as ICreateService)()
-  
+  const getService = (id: string, createService: () => IResizableContext) => {
+    serviceMap[id] = serviceMap[id] ? serviceMap[id] : createService()
+  }
 
-  const clearService = (id: string) => 
+  const clearService = (id: string) =>
     delete serviceMap[id]
-  
 
   return {
     getService,
