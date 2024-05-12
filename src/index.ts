01059134975fcc52ec9feb1303fac294f01d402b
BipanKishore <bipankishore@gmail.com>
2024-05-13 00:53:57 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ export {
   Pane
 } from './components/pane'
 
-export {
-  ResizablePaneProvider as ResizablePanes
-} from './components/resizable-pane-provider'
+// export {
+//   ResizablePaneProvider as ResizablePanes
+// } from './components/resizable-pane-provider'
 
 export {RATIO, BUTTOM_FIRST, TOP_FIRST} from './constant'
