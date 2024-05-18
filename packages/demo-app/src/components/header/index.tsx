24dbd593d3015b3637b93155105a61ca629e26ff
BipanKishore <bipankishore@gmail.com>
2024-05-18 14:51:55 +0530

    default lib name react

--- packages/demo-app/src/components/header/index.tsx
+++ packages/demo-app/src/components/header/index.tsx
@@ -8,14 +8,14 @@ import { getDetailsByLib } from '../demo-header/utils'
 const Header = () => {
   const { title, npmLink, libType } = getDetailsByLib()
   return (
-    <div className='header bg-gray-100 leading-9 p-1.5 mt-5 mb-10  rounded-xl py-3 px-2.5  top-0 z-40'>
+    <div className='header bg-gray-100 leading-9 mt-5 mb-10 rounded-xl py-2.5 px-2.5 top-0 z-40'>
       <div className='md:grid md:grid-cols-3'>
         <div />
         <h2 className='m0 text-center text-3xl'>{title}</h2>
 
-        <div className='grid place-center md:mt-0 mt-4'>
+        <div className='grid place-center md:mt-1.5 mt-4'>
           <Links selectedLib={libType} npmLink={npmLink} className='flex md:mr-2' />
-          </div>
+        </div>
       </div>
     </div>
   )
