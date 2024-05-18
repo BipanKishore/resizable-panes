83a010902647f8609fef569e9a1a7aa73560798d
BipanKishore <bipankishore@gmail.com>
2024-05-18 14:37:58 +0530

    lerna-commands

--- packages/resizable-panes-js/src/index.ts
+++ packages/resizable-panes-js/src/index.ts
@@ -1,7 +1,7 @@
 import {getResizablePanes} from './resizable-provider'
 // import {INITIAL_CONFIG, CHILD_CONFIG} from './utils'
 
-export {BUTTOM_FIRST, RATIO, TOP_FIRST} from '../../resizable-core'
+export {BUTTOM_FIRST, RATIO, TOP_FIRST} from '../../resizable-core/src'
 export default getResizablePanes
 
 // const [api] = getResizablePanes(INITIAL_CONFIG)
--- packages/resizable-panes-react/src/index.ts
+++ packages/resizable-panes-react/src/index.ts
@@ -6,4 +6,4 @@ export {
   ResizablePaneProvider as ResizablePanes
 } from './components/resizable-pane-provider'
 
-export {RATIO, BUTTOM_FIRST, TOP_FIRST} from '../../resizable-core'
+export {RATIO, BUTTOM_FIRST, TOP_FIRST} from '../../resizable-core/src'
