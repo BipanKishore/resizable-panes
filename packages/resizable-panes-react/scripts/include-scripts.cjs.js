a5a68e46068d6ff348defd5ba35b997a6422c7a8
BipanKishore <bipankishore@gmail.com>
2024-05-19 14:04:09 +0530

    cleaning

--- packages/resizable-panes-react/scripts/include-scripts.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cjs/resizable-panes-react.production.min.cjs.js')
-} else {
-  module.exports = require('./cjs/resizable-panes-react.development.cjs.js')
-}
